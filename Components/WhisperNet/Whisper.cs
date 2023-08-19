9dbb9988b7092c33df42c1c7bac0d7a83de2c7de
vladimirdesyat <vladimirdesyat@gmail.com>
2023-08-19 11:17:13 +0300

    removed unused info

--- Components/WhisperNet/Whisper.cs
+++ Components/WhisperNet/Whisper.cs
@@ -22,7 +22,7 @@ namespace VoiceToTextTgBot.Components.WhisperNet
             using var whisperFactory = WhisperFactory.FromPath("ggml-small.bin");
 
             using var processor = whisperFactory.CreateBuilder()
-            .WithLanguage("auto")
+            .WithLanguage("ru")
             .WithSpeedUp2x()
             .WithThreads(16)
             .Build();

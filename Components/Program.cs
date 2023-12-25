fb20780670d76e119141498397ed146a34b54467
vladimirdesyat <vladimirdesyat@gmail.com>
2023-12-25 19:17:54 +0300

    new func working with token swap

--- Components/Program.cs
+++ Components/Program.cs
@@ -9,15 +9,14 @@ namespace VoiceToTextTgBot.Components
     {
         static void Main()
         {
-            TelegramBot tgBot = null;
-            var cts = new CancellationTokenSource();
-
             while (true)
             {
                 Console.WriteLine("Type Telegram Bot Token:");
                 var token = Console.ReadLine();                
 
-                tgBot = new TelegramBot(token);                
+                var tgBot = new TelegramBot(token);
+
+                var cts = new CancellationTokenSource();
                 
                 var cancellationToken = cts.Token;
                 var receiverOptions = new ReceiverOptions
@@ -36,18 +35,16 @@ namespace VoiceToTextTgBot.Components
                 Console.WriteLine("Press 'Enter' to change the token or 'Ctrl+C' to exit.");
                 Console.ReadLine();
 
-                if (tgBot != null)
-                {
-                    // Stop the previous bot by canceling the token
-                    cts.Cancel();
+                // Stop the previous bot by canceling the token
+                cts.Cancel();
 
-                    // Optionally wait for a short time to ensure the bot is stopped
-                    Thread.Sleep(1000); // 1 second
+                // Optionally wait for a short time to ensure the bot is stopped
+                Thread.Sleep(1000); // 1 second
 
-                    // Dispose of the previous TelegramBot instance
-                    tgBot.Dispose();
-                }
+                // Dispose of the previous TelegramBot instance
+                tgBot.Dispose();
             }
         }
     }
 }
+
